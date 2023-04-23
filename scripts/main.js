f3d0ca0a65198d6dfe9ce4145aa08195f3864b6c
Itsadesign2022 <aa.design1921@gmail.com>
2023-04-22 22:28:21 -0400

    added date last modified

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
+// Last updated: April 22, 2023
+
+
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 
 const justYear = {year: 'numeric'};
 
-document.getElementById('update').textContent = new Date().toLocaleDateString('en-Us', options);
+const updateElement = document.getElementById("update");
+const lastModified = new Date(document.lastModified);
+updateElement.textContent = lastModified.toLocaleString();
+
 
 document.getElementById('current-year').textContent = new Date().toLocaleDateString('en-US', justYear);
