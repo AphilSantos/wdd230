c317dc8b02d002d903b6e60aea77e74517c12154
Itsadesign2022 <aa.design1921@gmail.com>
2023-04-22 22:08:09 -0400

    js and css

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,7 @@
-let fish = document.getElementById('update');
-fish.innerHTML = "seeing if js works";
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+
+const justYear = {year: 'numeric'};
+
+document.getElementById('update').textContent = new Date().toLocaleDateString('en-Us', options);
+
+document.getElementById('current-year').textContent = new Date().toLocaleDateString('en-US', justYear);
