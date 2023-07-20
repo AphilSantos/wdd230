2f1d6e0744ed64ed465060bec3a884a5b957dd85
AaronS <88414777+AphilSantos@users.noreply.github.com>
2023-07-20 01:59:38 -0400

    bountiful

---
+++ Bountiful/scripts/drinkorder.js
@@ -0,0 +1,99 @@
+const url = 'https://jd4440.github.io/wdd230/bountiful/json/data.json';
+    
+async function fruitFetch(url) {
+  try {
+      const response = await fetch(url);
+      if (response.ok) {
+          const data = await response.json();
+          console.log(data)
+
+      //Poppulate options
+      data.forEach((item)=>{
+        let fruit1 = document.getElementById("fruit1")
+        let fruit2 = document.getElementById("fruit2")
+        let fruit3 = document.getElementById("fruit3")
+        fruit1.innerHTML += `<option value = "${item.name}">${item.name}</option>`
+        fruit2.innerHTML += `<option value = "${item.name}">${item.name}</option>`
+        fruit3.innerHTML += `<option value = "${item.name}">${item.name}</option>`
+      })
+    }
+  }
+  catch(error){console.log(error)}
+}
+
+fruitFetch(url)
+
+async function  drinkCard() {
+  console.log("here")
+    // Get the current date
+    const orderDate = new Date().toDateString();
+    const firstName = document.getElementById('firstName').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const fruit1 = document.getElementById('fruit1').value;
+    const fruit2 = document.getElementById('fruit2').value;
+    const fruit3 = document.getElementById('fruit3').value;
+    const instructions = document.getElementById('instructions').value;
+
+    // Calculate the total nutrition values based on selected fruits
+
+    let data
+    try {
+      const  response = await fetch(url);
+      if (response.ok) {
+          data = await response.json();
+          console.log(data)
+      }
+    }catch(error){
+        console.log(error)
+      }
+    const selectedFruits = [fruit1, fruit2, fruit3];
+    let totalCarbs = 0, totalProtein = 0, totalFat = 0, totalSugar = 0, totalCalories = 0;
+    selectedFruits.forEach(fruitName => {
+      const fruit = data.find(fruit => fruit.name === fruitName);
+      if (fruit) {
+      
+        totalCarbs += fruit.nutritions.carbohydrates;
+        totalProtein += fruit.nutritions.protein;
+        totalFat += fruit.nutritions.fat;
+        totalSugar += fruit.nutritions.sugar;
+        totalCalories += fruit.nutritions.calories;
+        }
+      });
+
+      console.log(totalCarbs,totalProtein,totalFat,totalSugar,totalCalories)
+    
+    // Generate the formatted output
+    const output = `
+      
+      <h2><strong>First Name:</strong> ${firstName}</h2>
+      <h2><strong>Email:</strong> ${email}</h2>
+      <h2><strong>Phone Number:</strong> ${phone}</h2>
+      <h2><strong>Order Date:</strong> ${orderDate}</h2>
+      <h2><strong>Selected Fruits:</strong> ${fruit1 + " " + fruit2 + " " + fruit3}</h2>
+      <h2><strong>Special Instructions:</strong> ${instructions}</h2>
+      <h1>Total Nutrition:</h1>
+      <h2><strong>Carbohydrates:</strong> ${totalCarbs.toFixed(2)}g</h2>
+      <h2><strong>Protein:</strong> ${totalProtein.toFixed(2)}g</h2>
+      <h2><strong>Fat:</strong> ${totalFat.toFixed(2)}g</h2>
+      <h2><strong>Sugar:</strong> ${totalSugar.toFixed(2)}g</h2>
+      <h2><strong>Calories:</strong> ${totalCalories.toFixed(2)}kcal</h2>
+    `;
+      // Display the output
+      document.getElementById('output').innerHTML = output;
+
+      //Count number of orders
+      let numOrders = parseInt(localStorage.getItem("num-orders") || 0)+1;
+      localStorage.setItem("num-orders", numOrders);
+
+      
+    
+}
+
+
+
+document.querySelector("#drink-card-submit").addEventListener("click",drinkCard)
+
+
+
+
---
+++ Bountiful/scripts/weather.js
@@ -0,0 +1,82 @@
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const currentHumidity = document.querySelector('#current-hum');
+
+
+const currentweather = 'https://api.openweathermap.org/data/2.5/weather?lat=33.1580900&lon=-117.3505900&units=imperial&appid=93c38752305ca77c6133640779c32982';
+const threedayforecast = 'https://api.openweathermap.org/data/2.5/forecast?lat=33.1580900&lon=-117.3505900&units=imperial&appid=93c38752305ca77c6133640779c32982';
+
+async function apiFetch() {
+  try {
+      const response = await fetch(currentweather);
+      if (response.ok) {
+          const data = await response.json();
+
+          displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `${weatherData.main.temp.toFixed(1)}`; 
+  humidity.innerHTML = `${weatherData.main.humidity}`;
+    
+  const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+}
+
+
+async function threeDay () {
+   let data
+
+   let forecastDate = new Date()
+   let arrayDays = [(new Date(forecastDate.setDate(forecastDate.getDate()+1))).toLocaleDateString('en-US'),
+                    new Date(forecastDate.setDate(forecastDate.getDate()+1)).toLocaleDateString('en-US'),
+                    new Date(forecastDate.setDate(forecastDate.getDate()+1)).toLocaleDateString('en-US')
+                    ]
+  console.log(arrayDays)
+    try {
+        const response = await fetch(threedayforecast);
+        if (response.ok) {
+            data = await response.json();
+            
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    let forecastObjects = []
+
+    forecastObjects = data.list.filter((item) => {
+    
+      return arrayDays.includes(new Date(item.dt_txt).toLocaleDateString("en-US")) && new Date(item.dt_txt).getHours() === 12;
+    
+    });
+    let days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    document.querySelector("#dayone-temp").textContent = forecastObjects[0].main.temp
+    document.querySelector("#daytwo-temp").textContent = forecastObjects[1].main.temp
+    document.querySelector("#daythree-temp").textContent = forecastObjects[2].main.temp
+    
+    document.querySelector("#dayone-date").textContent = days[(new Date(arrayDays[0]).getDay())]
+    document.querySelector("#daytwo-date").textContent = days[(new Date(arrayDays[1]).getDay())]
+    document.querySelector("#daythree-date").textContent = days[(new Date(arrayDays[2]).getDay())]
+  
+}
+threeDay()
+
+apiFetch();
+
+
+
